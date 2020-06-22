d63c0bdf5461d70bbb03efd6efc4a6cbc208fb38
rishad190 <66918654+rishad190@users.noreply.github.com>
2020-06-22 16:58:21 +0600

    Add files via upload

---
+++ src/robots.js
@@ -0,0 +1,54 @@
+export const robots = [
+  {  id:1,
+    name: 'Dam Care',
+    username: 'Dam',
+    email: 'damcare@gmail.com'
+},
+{
+    id: 2,
+    name: 'Alu aushura',
+    username: 'alu',
+    email: 'aluashura@gmail.com'
+},
+
+{
+    id:3,
+    name: 'Moha gheni',
+    username: 'gheni',
+    email: 'mohagheni@gmail.com'
+},
+{
+    id:4,
+    name: 'Number one',
+    username: 'dudu',
+    email: 'numberone@gmail.com'
+},
+{  id:1,
+    name: 'Dam Care',
+    username: 'Dam',
+    email: 'damcare@gmail.com'
+},
+{
+    id: 2,
+    name: 'Alu aushura',
+    username: 'alu',
+    email: 'aluashura@gmail.com'
+},
+
+{
+    id:3,
+    name: 'Moha gheni',
+    username: 'gheni',
+    email: 'mohagheni@gmail.com'
+},
+{
+    id:4,
+    name: 'Number one',
+    username: 'dudu',
+    email: 'numberone@gmail.com'
+}
+
+
+
+
+]
